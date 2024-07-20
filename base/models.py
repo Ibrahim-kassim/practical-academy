6442235d1f8deed23f34dd9c415eb289f3eab97d
Your Name <you@example.com>
2024-07-20 12:05:46 +0300

    session 2

--- base/models.py
+++ base/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    
+    def __str__(self) :
+        return self.name
+
+class Room(models.Model):
+    host = models.ForeignKey(User,on_delete=models.SET_NULL , null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self) :
+        return self.name
+
+class Message(models.Model):
+    host = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    room = models.ForeignKey(Room,on_delete=models.CASCADE,null=True)
+
+    body = models.TextField()
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    
+    def __str__(self) :
+        return self.body
+
--- base/views.py
+++ base/views.py
@@ -1,24 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
-rooms = [
-    {'id': 1, 'name': 'Python Basics', 'description': 'Learn the fundamentals of Python programming'},
-    {'id': 2, 'name': 'Django Framework', 'description': 'Explore web development with Django'},
-    {'id': 3, 'name': 'Data Science', 'description': 'Dive into data analysis and machine learning'},
-    {'id': 4, 'name': 'Web Design', 'description': 'Master HTML, CSS, and responsive design'},
-]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {"leen":rooms}
     return render(request,"base/home.html",context)
 
 def room(request,pk):
-    r = None
-    for ro in rooms:
-        if ro['id'] == int(pk):
-            r = ro
+    # r = None
+    # for ro in rooms:
+    #     if ro['id'] == int(pk):
+    #         r = ro
+    r = Room.objects.get(id=pk)
     context ={"room":r}        
     return render(request,"base/room.html",context)
 
