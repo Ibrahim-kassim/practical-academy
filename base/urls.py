13f1cdc246846bae5218264a23cc5da4fd21a53d
Your Name <you@example.com>
2024-07-27 10:47:12 +0300

    create operation

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("",views.home,name="home"),
     path("room/<str:pk>",views.room,name="room"),
     path("contact/",views.contactus,name="contact"),
+    path("create-room/",views.createRoom,name="create-room")
     
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 from django.http import HttpResponse
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -21,3 +22,13 @@ def room(request,pk):
 
 def contactus(request):
     return render(request,"base/contactus.html")
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {"form":form}
+    return render(request,"base/room_form.html",context)
